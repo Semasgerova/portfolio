b9e0a8120d6a9f78be40414b41b568f5b552104c
Sema <semaesgerova68@gmail.com>
2023-08-02 00:57:06 +0400

    cursor design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Home from './Home'
 
 const App = () => {
+
+  const [cursorX, setCursorX] = useState();
+  const [cursorY, setCursorY] = useState();
+
+  window.addEventListener('mousemove',(e)=>{
+      setCursorX(e.pageX)
+      setCursorY(e.pageY)
+  })
+
   return (
     <>
+      <div className="cursor"
+      style={{left:cursorX + "px", top:cursorY + "px"}}
+      ></div>
       <Header/>
       <Home/>
     </>
